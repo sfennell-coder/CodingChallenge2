80395e8892521d8c035319f6fd42a3bafe0b11ea
Scott Fennell <slfennell01@gmail.com>
2024-05-15 18:07:09 -0400

    Create Car class with constructor for make and speed
    Create Car class with constructor for make and speed

---
+++ cc_2.js
@@ -0,0 +1,12 @@
+// Scott Fennell (U86364709)
+// CC_1 Car Class Implementation Challenge
+
+/*1. Implement a car class with constructors that 
+initializes the make an speed of car*/
+
+class car {
+    constructor(make, speed){
+        this.make = make;
+        this.speed = speed;
+    }
+}
