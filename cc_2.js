120e9f01fd5902df740408eb38609a2f17a314f1
Scott Fennell <slfennell01@gmail.com>
2024-05-16 18:18:21 -0400

    Add brake method to decrease speed by 5 km/h
    Add brake method to decrease speed by 5 km/h

--- cc_2.js
+++ cc_2.js
@@ -9,8 +9,14 @@ class car {
         this.make = make;
         this.speed = speed;
     }
+    //2.)Create a method for accelerating the make of vehicle by 10 km/h
     accelerate(){
         this.speed += 10;
-        console.log(this.make, " accelerated. New speed: ", this.speed, " km/h");
+        console.log("${this.make} accelerated. New speed: ${this.speed} km/h");
+    }
+    //3.)Create a method for braking
+    brake(){
+        this.speed -= 5;
+        console.log("${this.make} braked. New speed: ${this.speed} km/h");
     }
 }
